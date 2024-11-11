d4e46181e1da072c2c36b74db906cecf429c9598
Emiliano Barroso <barrosoemiliano3@gmail.com>
2024-11-11 13:15:15 -0300

    Subiendo Proyecto

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>TecnoShop</h1>
+            <p>Transforma tu mundo digital</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
